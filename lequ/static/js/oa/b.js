7e572502f5415712c49003ac4559e0351285dd81
Gao Peng <gaopeng@wisdomgarden.com>
2016-01-26 15:32:56 +0800

    rm lequ/static added by mistake

--- lequ/static/js/oa/b.js
+++
@@ -1,2 +0,0 @@
-var t=1;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImIuanMiXSwibmFtZXMiOlsidCJdLCJtYXBwaW5ncyI6IkFBQUEsR0FBSUEsR0FBRSIsImZpbGUiOiJiLmpzIiwic291cmNlc0NvbnRlbnQiOlsidmFyIHQ9MTsiXSwic291cmNlUm9vdCI6Ii9zb3VyY2UvIn0=
