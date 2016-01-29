742b41d573921473b61c59c1e764a2ae2354fbe5
Gao Peng <gaopeng@wisdomgarden.com>
2016-01-29 21:56:43 +0800

    fix typos

--- config/default.py
+++ config/default.py
@@ -5,5 +5,5 @@ SECRET_KEY = "234523453tsfgdfgsdgfw345g"
 
 MAIL_FROM_EMAIL = "robert@example.com"
 SQLALCHEMY_DATABASE_URI = 'mysql://root:@localhost/lequ_dev?charset=utf8'
-QLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_ECHO = True
